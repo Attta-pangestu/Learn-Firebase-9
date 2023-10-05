f96dab5b85c5c5c499f392c2fffa92226c3b24ba
Attta-pangestu <athabarokahfc@gmail.com>
2023-10-05 15:38:28 +0000

    menggunakan webpack bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path') ; 
 module.exports = {
     mode :'development', 
-    entry : path.resolve(__dirname, 'src/views/index.html')
+    entry : path.resolve(__dirname, 'src/js/index.js'), 
+    output : {
+        path : path.resolve(__dirname, 'dist'), 
+        filename : 'bundle.js'
+    }  
 }
